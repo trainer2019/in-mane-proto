7846a43358fd029ad2932c8dfb770a85a4f03792
masahiro-ikeda <masahiro.ikeda@careritz.co.jp>
2019-03-01 18:43:38 +0900

    oracleの接続先をrdsに変更

--- src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO usr (user_id, first_name, last_name, role_name, password)
-VALUES ('taro-yamada', '太郎', '山田', 'USER', '$2a$10$oxSJl.keBwxmsMLkcT9lPeAIxfNTPNQxpeywMrF7A3kVszwUTqfTK');
-INSERT INTO usr (user_id, first_name, last_name, role_name, password)
-VALUES ('aaaa', 'Aaa', 'Aaa', 'USER', '$2a$10$oxSJl.keBwxmsMLkcT9lPeAIxfNTPNQxpeywMrF7A3kVszwUTqfTK');
-INSERT INTO usr (user_id, first_name, last_name, role_name, password)
-VALUES ('bbbb', 'Bbb', 'Bbb', 'USER', '$2a$10$oxSJl.keBwxmsMLkcT9lPeAIxfNTPNQxpeywMrF7A3kVszwUTqfTK');
-INSERT INTO usr (user_id, first_name, last_name, role_name, password)
-VALUES ('cccc', 'Ccc', 'Ccc', 'ADMIN', '$2a$10$oxSJl.keBwxmsMLkcT9lPeAIxfNTPNQxpeywMrF7A3kVszwUTqfTK');

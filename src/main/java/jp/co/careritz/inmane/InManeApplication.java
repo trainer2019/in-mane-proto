5c4e82c9694a252ddcc898a7adddd4477af9eb87
masahiro-ikeda <masahiro.ikeda@careritz.co.jp>
2019-03-01 18:32:01 +0900

    外部のAPサーバにデプロイできるように起動処理を修正

--- src/main/java/jp/co/careritz/inmane/InManeApplication.java
+++ src/main/java/jp/co/careritz/inmane/InManeApplication.java
@@ -2,11 +2,18 @@ package jp.co.careritz.inmane;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class InManeApplication {
+public class InManeApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InManeApplication.class, args);
+	}
+
+	@Override
+	public SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(InManeApplication.class);
 	}
 }
